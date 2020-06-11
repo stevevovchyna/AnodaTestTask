344aace932e15af71f302f91017569addfdbe701
Stepan Vovchyna <steve.vovchyna@gmail.com>
2020-06-11 18:12:07 +0300

    Initial Commit

---
+++ AnodaTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnodaTest
+//
+//  Created by Steven Vovchyna on 11.06.2020.
+//  Copyright © 2020 Steven Vovchyna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
